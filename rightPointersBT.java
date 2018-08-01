f3bda96f3a46f3d33c5e94e09c708605dda33f84
Rosa Mohammadi <rosamohammadi@alumni.ubc.ca>
2018-08-01 11:34:30 -0700

    leetcode 116

---
+++ rightPointersBT.java
@@ -0,0 +1,56 @@
+
+/* LEETCODE #116 */
+
+/*
+
+Given a binary tree
+
+struct TreeLinkNode {
+TreeLinkNode *left;
+TreeLinkNode *right;
+TreeLinkNode *next;
+}
+
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Note:
+
+You may only use constant extra space.
+Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+
+*/
+
+
+
+/**
+* Definition for binary tree with next pointer.
+* public class TreeLinkNode {
+*     int val;
+*     TreeLinkNode left, right, next;
+*     TreeLinkNode(int x) { val = x; }
+* }
+*/
+
+
+public class Solution {
+
+  public void connect(TreeLinkNode root) {
+    if(root == null)
+    return;
+
+    // given that tree is perfect, if a left node exists, a right node must also
+    if(root.left != null){
+      root.left.next = root.right;
+      connect(root.left);
+      if(root.next != null) {
+        root.right.next = root.next.left;
+      }
+      connect(root.right);
+    }
+
+  }
+  
+}
